f9278a8c868d60d83268e3fa622742b8f8219f70
A H M E D <70113157+ahmed-03@users.noreply.github.com>
2024-12-10 16:17:15 +0300

    Update Shift_Reg.sv

--- Shift_Reg.sv
+++ Shift_Reg.sv
@@ -6,27 +6,28 @@ module Shift_Reg#(parameter width = 8)(
     input load,
     input right_left,
     input [width-1:0]data_in,
-    output logic [width-1:0]data_out,
     output logic out
     );
-
+    
+    logic [width-1:0]data_out;
     always @(posedge clk, negedge rst_n) begin
         if(!rst_n) begin
             data_out<='0;
+            out<=0;
         end
-        else if (en_shift)begin
-            if(load) begin
-                data_out<=data_in;
-            end
-            //right_left =1 right
-            else if (right_left) begin
-                out = data_out[0];
-                data_out<= data_out >>1;
-            end
-            //right_left =0 left
-            else if (!right_left) begin
-                out = data_out[width-1];
-                data_out<= data_out <<1;
+        else if (load)begin
+            data_out<=data_in;
+        else if(en_shift) begin
+                //right_left =1 right
+                if (right_left) begin
+                    out <= data_out[0];
+                    data_out<= data_out >>1;
+                end
+                //right_left =0 left
+                else if (!right_left) begin
+                    out <= data_out[width-1];
+                    data_out<= data_out <<1;
+                end
             end
         end
     end
